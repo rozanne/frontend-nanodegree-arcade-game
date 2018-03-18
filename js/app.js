e5e341212d81094d99062215664b9012f544e926
readm2 <readm2@readm2ui-MacBook-Pro.local>
2018-03-18 18:31:50 +0900

    Add new game

--- js/app.js
+++ js/app.js
@@ -6,7 +6,21 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    // set speed randomly
+    this.speed = (Math.random() + 1) * 200;
+
+    // set Initial prosition randomly
+    this.initPosition();
 };
+
+Enemy.prototype.initPosition = function() {
+    var maxRandom = 4;
+    var minRandom = 0;
+
+    this.x = -400 + (Math.floor((Math.random() * maxRandom) + minRandom) * 100);
+    this.y = (Math.floor((Math.random() * maxRandom) + minRandom) * 90) + 50;
+}
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,6 +28,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    // if enemy is out of screen, it is called init again
+    if(this.x > 505) {
+        this.initPosition();
+    }
+    this.x += dt * this.speed;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +44,118 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    // The image/sprite for our player, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+
+    // set stride
+    this.strideX = 101;
+    this.strideY = 83;
+
+    // set boundery of canvas
+    this.bound = {
+        left: 0,
+        right: 505,
+        up: -50,
+        down: 450
+    }
+
+    // set finishLine position
+    this.finishLine = 0;
+
+    // locate player at the center
+    this.initPosition();
+
+    // for moving position
+    this.moveX = this.x;
+    this.moveY = this.y;
+};
+
+Player.prototype.initPosition = function() {
+    this.x = 202;
+    this.y = 400;
+}
+
+// Update the player's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x = this.moveX;
+    this.y = this.moveY;
+};
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Handle input for moving player with key event
+Player.prototype.handleInput = function(dir) {
+    if(isFinishGame) {
+        return;
+    }
+    switch (dir) {
+        case 'left':
+            console.log('left');
+            var x = this.x - this.strideX;
+            if(x >= this.bound.left) {
+                this.moveX = x;
+            }
+            break;
+        case 'up':
+            console.log('up');
+            var y = this.y - this.strideY;
+            if(y >= this.bound.up) {
+                this.moveY = y;
+                if(y < this.finishLine) {
+                    finishGame();
+                }
+            }
+            break;
+        case 'right':
+            console.log('right');
+            var x = this.x + this.strideX;
+            if(x < this.bound.right) {
+                this.moveX = x;
+            }
+            break;
+        case 'down':
+            console.log('down');
+            var y = this.y + this.strideY;
+            if(y <= this.bound.down) {
+                this.moveY = y;
+            }
+            break;
+        default:
+
+    }
+};
+
+var isFinishGame = false;
+function finishGame() {
+    updateNotice('Finally you win!!! Bugs are all disappeared!');
+    isFinishGame = true;
+    allEnemies = [];
+}
+
+function updateNotice(msg) {
+    document.getElementById('notice').innerText = msg;
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var enemyNum = 5;
+var allEnemies = [];
+for(var i = 0; i < enemyNum; i++) {
+    allEnemies[i] = new Enemy();
+}
 
-
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
